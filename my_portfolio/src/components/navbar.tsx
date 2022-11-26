aa117baea9bfe220895258112e4a8d63e32d8ea8
sanket <programmersanket@gmail.com>
2022-11-26 19:48:16 +0530

    Working Started on Portfolio with typescript in react

---
+++ my_portfolio/src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const NavBar = () => {
+    return ( 
+        <div className='nav'>
+            <div className="logo">
+
+            </div>
+            <div className="menues">
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default NavBar;
